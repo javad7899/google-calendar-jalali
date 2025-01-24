9730f697705a535a9b4ec447f2dd75e03524ba8b
javad7899 <javadhashemzadeh78@gmail.com>
2025-01-24 09:42:05 +0330

    fix backend configs

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://google-calendar-fe.chbk.app"],
     credentials: true,
   })
 );
